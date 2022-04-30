270054213b5d6848702fcc118c9be9baa360cda8
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-04-30 19:09:48 +0200

    better logic to identify untrusted variables at the beginning

---
+++ project/test_contracts/derivation.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract Contract {
+  function bar(int x) public {
+    int a = 0;
+    a = x;
+    int b = x + 8;
+    int c = 2;
+    c = a + 1;
+    int d = 0;
+    d = b;
+    d = 3;
+  }
+}

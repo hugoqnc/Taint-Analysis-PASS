03c79ce116b3f7b2f27e008784b6f26d6dea5a97
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-04-30 19:43:46 +0200

    first try to identify actual guards

--- project/test_contracts/derivation.sol
+++ project/test_contracts/derivation.sol
@@ -1,14 +1,25 @@
 pragma solidity ^0.5.0;
 
 contract Contract {
-  function bar(int x) public {
-    int a = 0;
-    a = x;
-    int b = x + 8;
-    int c = 2;
-    c = a + 1;
-    int d = 0;
-    d = b;
-    d = 3;
+  int x;
+  int y;
+
+//  function bar(int x) public {
+//    int a = 0;
+//    a = x;
+//    int b = x + 8;
+//    int c = 2;
+//    c = a + 1;
+//    int d = 0;
+//    d = b;
+//    d = 3;
+//  }
+  function foo(int a) public {
+    x = a;
+    int b = x;
+    int c = b;
+    y = c;
+    x = 0;
+    int d = x;
   }
 }

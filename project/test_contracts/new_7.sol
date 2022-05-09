4c227753451873e01ed9e6bb30ae3e3776c940cb
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-05-09 10:52:48 +0200

    current progress

--- project/test_contracts/new_7.sol
+++ project/test_contracts/new_7.sol
@@ -1,25 +1,17 @@
 pragma solidity ^0.5.0;
 
 contract Contract {
-  address a;
   address c;
-  address d;
-
-  function order(address payable addr) public {
-    require(msg.sender==c); // guard
-    selfdestruct(addr); // safe
-  }
 
   function order2(address addr) public {
-    a = addr;
     address payable b1 = msg.sender;
-    address dependOnSender;
-    if(b1==a) {
+    address dependOnSender = addr;
+    if(b1==addr) {
       dependOnSender = addr; // depends on msg.sender
       require(dependOnSender==c); // guard
       selfdestruct(b1); // safe
     }
-    require(d==c); // not a guard
+    require(dependOnSender==c); // not a guard
     selfdestruct(b1); // vulnerable
   }
 }
